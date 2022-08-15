7cad46001ac4c066807f15c867374cacc5a6ff73
Teja <tejanuthakki12@gmail.com>
2022-08-15 15:53:56 +0530

    First test Commit

---
+++ src/main/java/com/teja/PostgreeCrud/PostgreeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.teja.PostgreeCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teja/PostgreeCrud/PostgreeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teja.PostgreeCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
